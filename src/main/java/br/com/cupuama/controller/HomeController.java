b3875011cb62c8caf30316c9b8aa57d1b617542d
Romeu Franzoia <rfranzoia@gmail.com>
2021-03-21 18:16:18 +0000

    added cross origin config

--- src/main/java/br/com/cupuama/controller/HomeController.java
+++ src/main/java/br/com/cupuama/controller/HomeController.java
@@ -1,11 +1,13 @@
 package br.com.cupuama.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 @ApiIgnore
+@CrossOrigin
 public class HomeController
 {
 
